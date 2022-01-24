083192462fd1a0cc7045eef0424c17651a32bb39
Folau Kaveinga <folaukaveinga@gmail.com>
2022-01-24 11:32:55 -0700

    setup

---
+++ springboot-with-oauth2/src/main/java/com/lovemesomecoding/oauth2/SpringbootWithOauth2Application.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithOauth2Application.class, args);
+	}
+
+}
